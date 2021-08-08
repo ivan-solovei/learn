1e450148d9cce12963c458b71dad6c4de76388b7
Ivan Solovei <soloway.ivan.412@gmail.com>
2021-08-08 20:36:47 +0300

    vers1

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,34 @@
-// function myMap() {
-//   var mapCanvas = document.getElementById("mapUkr");
-//   var mapOptions = {
-//       center: new google.maps.LatLng(51.5, -0.2), zoom: 10
-//   };
-//   var map = new google.maps.Map(mapCanvas, mapOptions);
-// }
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger,.nav__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+
+function navMenu(selector) {
+    let menu =$(selector);
+    let button = menu.find('.header__burger');
+    let links = menu.find('.nav__menu');
+    let overlay = menu.find('.header__menu__overlay');
+
+
+button.on('click', (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+  
+  links.on('click', () => toggleMenu());
+  overlay.on('click', () => toggleMenu());
+  
+  function toggleMenu(){
+    menu.toggleClass('.active');
+    
+    if (menu.hasClass('.active')) {
+      $('body').css('overlow', 'hidden');
+    } else {
+      $('body').css('overlow', 'visible');
+    }
+  }
+}
+
+headerBurger('.header__burger');
